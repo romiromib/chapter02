85a54de2f8e36945055c3d379d0b0f996807ade9
romiromib <raspberry.b85@gmail.com>
2024-01-02 16:16:51 +0900

    클래스의 정의

---
+++ src/com/javaex/ex01/Goods.java
@@ -0,0 +1,12 @@
+package com.javaex.ex01;
+
+public class Goods {
+	//필드
+	String name;
+	int price;
+	
+	
+	//생성자
+	//메소드-g/s
+	// -일반
+}
---
+++ src/com/javaex/ex02/Goods.java
@@ -0,0 +1,13 @@
+package com.javaex.ex02;
+
+public class Goods {
+
+	String name;
+	int price;
+	
+	//필드
+	//생성자
+	//메소드 g/s
+	//	   일반
+	
+}
